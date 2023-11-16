784b66ed6f71bc07d1aa70d936836db5a8aa046d
Lukey <lucianopozzoli@icloud.com>
2023-11-15 23:33:24 -0300

    Implementacion de nodos y variables

--- RedSocial.h
+++ RedSocial.h
@@ -5,26 +5,38 @@
 #include <map>
 #include <unordered_map>
 #include <set>
+using namespace std;
 
 class RedSocial{
   public:
     RedSocial();
 
-    const std::set<int> & usuarios() const; // O(1)
-    std::string obtener_alias(int id) const; // O(log n)
-    const std::set<std::string> & obtener_amigos(int id) const; // O(log n)
-    const std::set<std::string> & obtener_conocidos(int id) const; // O(log n)
+    const set<int> & usuarios() const; // O(1)
+    string obtener_alias(int id) const; // O(log n)
+    const set<string> & obtener_amigos(int id) const; // O(log n)
+    const set<string> & obtener_conocidos(int id) const; // O(log n)
     int cantidad_amistades() const; // O(1)
 
-    void registrar_usuario(std::string alias, int id); // O(log n) + O(1) promedio
+    void registrar_usuario(string alias, int id); // O(log n) + O(1) promedio
     void eliminar_usuario(int id); // sin requerimiento
     void amigar_usuarios(int id_A, int id_B); // sin requerimiento
     void desamigar_usuarios(int id_A, int id_B); // sin requerimiento
     
-    int obtener_id(std::string alias) const; // O(1) promedio
-    const std::set<std::string> & conocidos_del_usuario_mas_popular() const; // O(1)
+    int obtener_id(string alias) const; // O(1) promedio
+    const set<string> & conocidos_del_usuario_mas_popular() const; // O(1)
 
   private:
+  struct node {
+    int id;
+    string alias;
+    node* izquierdo;
+    node* derecho;
+  };
+
+  set<int,int> _amistades;
+  int _cantidad_amistades;
+  set<string> _conocidos_mas_popular;
+
     /* completar... */
 };
 
